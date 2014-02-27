b8e374f050ca7167478a1e7841d7fd3389a0582b
Rimina Angelica <riminarchive@gmail.com>
2014-02-28 06:54:01 +0800

    Fixed Sign Up and content layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	  def configure_permitted_parameters
-	    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :contact_number, :profile_picture, :handle) }
-	    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :contact_number, :profile_picture, :handle, :current_password) }
+	    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :contact_number, :profile_picture, :handle, :email, :password, :password_confirmation) }
+	    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :contact_number, :profile_picture, :handle, :current_password, :email, :password, :password_confirmation) }
 	  end
 end
