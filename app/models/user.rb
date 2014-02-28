d976911335239ea92465ce68bb5beb479c6ba3ee
Rimina Angelica <riminarchive@gmail.com>
2014-02-28 08:30:45 +0800

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
  	# Setup accessible (or protected) attributes for your model
  	attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :handle, :profile_picture, :contact_number, :current_password
  	# attr_accessible :title, :body
+ 	
+ 	validates :first_name, presence: true
+ 	validates :last_name, presence: true
+ 	validates :handle, presence: true, uniqueness: true
+ 	validates :email, presence: true, uniqueness: true
+
  	has_many :entries
 
  	def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
 end
