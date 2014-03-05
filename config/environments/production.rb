77534e087c05405fa52d9a68ca211407f9adbae4
Rimina Angelica <riminarchive@gmail.com>
2014-03-06 01:51:13 +0800

    Fixes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,12 @@ Happygram::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.assets.precompile += ['active_admin.css']
+
+  config.action_mailer.default_url_options = {:host => 'happygram.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'happygram.herokuapp.com'
+  }
 end
