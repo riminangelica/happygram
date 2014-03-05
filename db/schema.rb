ba16763f2766b824be3567585bd967a14c74b865
Rimina Angelica <riminarchive@gmail.com>
2014-03-05 13:26:50 +0800

    Fixes for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304131659) do
+ActiveRecord::Schema.define(version: 20140305040604) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -54,6 +54,18 @@ ActiveRecord::Schema.define(version: 20140304131659) do
     t.datetime "updated_at"
   end
 
+  create_table "documents", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
+  end
+
+  add_index "documents", ["user_id"], name: "index_documents_on_user_id"
+
   create_table "entries", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -61,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140304131659) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "document_id"
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
@@ -79,6 +92,13 @@ ActiveRecord::Schema.define(version: 20140304131659) do
 
   add_index "friendships", ["state"], name: "index_friendships_on_state"
   add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id"
+
+  create_table "statuses", force: true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
